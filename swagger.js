98a808090fe38e59cc6f21e8dc49d2c754082f2d
gargtanishka330-droid <gargtanishka330@gmail.com>
2026-07-27 17:02:53 +0530

    Add .gitignore

--- swagger.js
+++ swagger.js
@@ -13,7 +13,7 @@ const options = {
 
     servers: [
       {
-      //  url: "http://localhost:5000",
+     
       url:"https://online-exam-portal-4-n36e.onrender.com",
         description: "Local Server",
       },
